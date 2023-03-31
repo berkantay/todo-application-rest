10e1dc70a14793c2298ac89759a76e82a3f20d94
Berkant Ay <berkant497@gmail.com>
2023-03-31 15:38:39 +0300

    password checking added

--- user/user.go
+++ user/user.go
@@ -2,8 +2,13 @@ package user
 
 import (
 	"context"
+	"errors"
 
 	"github.com/berkantay/todo-app-example/model"
+)
+
+const (
+	minPasswordLength = 8
 )
 
 type Repository interface {
@@ -25,6 +30,15 @@ func NewService(ctx context.Context, repository Repository) *Service {
 }
 
 func (s *Service) Create(ctx context.Context, user *model.User) (*model.User, error) {
+	if !isValidUsername(*user.Username) {
+		return nil, errors.New("username cannot be empty")
+	}
+
+	err := isValidPassword(*user.Password)
+	if err != nil {
+		return nil, err
+	}
+
 	createdUser, err := s.repository.Create(ctx, user)
 	if err != nil {
 		return nil, err
@@ -41,6 +55,9 @@ func (s *Service) Read(ctx context.Context, id string) ([]*model.User, error) {
 }
 
 func (s *Service) UpdateUsername(ctx context.Context, id string, username string) (*model.User, error) {
+	if !isValidUsername(username) {
+		return nil, errors.New("username cannot be empty")
+	}
 	user, err := s.repository.UpdateUsername(ctx, id, username)
 	if err != nil {
 		return nil, err
@@ -49,6 +66,10 @@ func (s *Service) UpdateUsername(ctx context.Context, id string, username string
 }
 
 func (s *Service) UpdatePassword(ctx context.Context, id string, password string) (*model.User, error) {
+	err := isValidPassword(password)
+	if err != nil {
+		return nil, err
+	}
 	user, err := s.repository.UpdatePassword(ctx, id, password)
 	if err != nil {
 		return nil, err
@@ -60,6 +81,17 @@ func (s *Service) Delete(ctx context.Context, id string) error {
 	err := s.repository.Delete(ctx, id)
 	if err != nil {
 		return err
+	}
+	return nil
+}
+
+func isValidUsername(username string) bool {
+	return username != ""
+}
+
+func isValidPassword(password string) error {
+	if len(password) < minPasswordLength {
+		return errors.New("password should be longer than 8 characters")
 	}
 	return nil
 }
